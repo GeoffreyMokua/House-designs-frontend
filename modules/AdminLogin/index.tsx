40857fee69c39a36fde34dea851153dee79a7097
Nicholas Njeru <nicholasnjeru917@gmail.com>
2023-09-21 18:54:14 +0300

    updates

--- modules/AdminLogin/index.tsx
+++ modules/AdminLogin/index.tsx
@@ -3,7 +3,6 @@ import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
 import * as yup from "yup";
@@ -40,16 +39,6 @@ const AdminLogin = () => {
         router.push("/admin");
       });
   };
-
-  useEffect(() => {
-    // Check if the user is authenticated (e.g., by verifying the JWT token in local storage)
-    const isAuthenticated = localStorage.getItem("jwtToken");
-
-    if (!isAuthenticated) {
-      // If not authenticated, redirect to the login page
-      router.push("/admin");
-    }
-  }, []);
 
   return (
     <Stack
